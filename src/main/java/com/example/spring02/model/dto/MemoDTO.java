3494b64becc1707fd26fedd22b4b7b46f70167bd
Pulse-less <39909902+Pulse-less@users.noreply.github.com>
2019-01-11 09:28:32 +0900

    Initial commit

---
+++ src/main/java/com/example/spring02/model/dto/MemoDTO.java
@@ -0,0 +1,10 @@
+package com.example.spring02.model.dto;
+
+import java.util.Date;
+
+public class MemoDTO {
+    private int idx;
+    private String writer;
+    private String memo;
+    private Date post_date;
+}
